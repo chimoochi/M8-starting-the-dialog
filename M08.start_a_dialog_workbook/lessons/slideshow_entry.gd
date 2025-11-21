8702577e86b9a24ab125a2d3794eb04f934fff5a
Timothy  Nguyen <timothysvn@gmail.com>
2025-11-21 15:48:32 -0800

    complete M8L5

---
+++ M08.start_a_dialog_workbook/lessons/slideshow_entry.gd
@@ -0,0 +1,5 @@
+class_name SlideShowEntry extends Resource
+
+@export_multiline var text := ""
+@export var expression := preload("res://assets/emotion_regular.png")
+@export var character := preload("res://assets/sophia.png")
